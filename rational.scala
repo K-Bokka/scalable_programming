5b3da06783274b52a5d2461ef0fff062f48706d7
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2018-12-30 12:45:47 +0900

    Add Rational.add method

--- rational.scala
+++ rational.scala
@@ -1,5 +1,14 @@
 class Rational(n: Int, d: Int) {
   require(d != 0)
 
-  override def toString: String = n + "/" + d
+  val numer: Int = n
+  val denom: Int = d
+
+  override def toString: String = numer + "/" + denom
+
+  def add(that: Rational): Rational =
+    new Rational(
+      numer * that.denom + that.numer * denom,
+      denom * that.denom
+    )
 }
