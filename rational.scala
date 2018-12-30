43120b6dcb29cdeec287db5c234d1c0ec4eb49ca
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2018-12-30 13:21:47 +0900

    Add substraction and division

--- rational.scala
+++ rational.scala
@@ -18,11 +18,26 @@ class Rational(n: Int, d: Int) {
   def +(i: Int): Rational =
     new Rational(numer + i * denom, denom)
 
+  def -(that: Rational): Rational =
+    new Rational(
+      numer * that.denom - that.numer * denom,
+      denom * that.denom
+    )
+
+  def -(i: Int): Rational =
+    new Rational(numer - i * denom, denom)
+
   def *(that: Rational): Rational =
     new Rational(numer * that.numer, denom * that.denom)
 
   def *(i: Int): Rational =
     new Rational(numer * i, denom)
+
+  def /(that: Rational): Rational =
+    new Rational(numer * that.denom, denom * that.numer)
+
+  def /(i: Int): Rational =
+    new Rational(numer, denom * i)
 
   private def gcd(a: Int, b: Int): Int =
     if (b == 0) a else gcd(b, a % b)
