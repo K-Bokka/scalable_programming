4fe90bfd0d8729f3a2ed5fc4822a3ff985f52db5
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2018-12-30 13:02:02 +0900

    Add reduction

--- rational.scala
+++ rational.scala
@@ -1,8 +1,9 @@
 class Rational(n: Int, d: Int) {
   require(d != 0)
 
-  val numer: Int = n
-  val denom: Int = d
+  private val g = gcd(n.abs, d.abs)
+  val numer: Int = n / g
+  val denom: Int = d / g
 
   def this(n: Int) = this(n, 1)
 
@@ -13,4 +14,7 @@ class Rational(n: Int, d: Int) {
       numer * that.denom + that.numer * denom,
       denom * that.denom
     )
+
+  private def gcd(a: Int, b: Int): Int =
+    if (b == 0) a else gcd(b, a % b)
 }
