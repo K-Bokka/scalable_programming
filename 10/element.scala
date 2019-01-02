d683321025c18a6378409f13baf60311e524f9c1
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2019-01-02 19:32:44 +0900

    Add factory method

--- 10/element.scala
+++ 10/element.scala
@@ -1,3 +1,5 @@
+import Element.elem
+
 abstract class Element {
   def contents: Array[String]
 
@@ -5,15 +7,23 @@ abstract class Element {
 
   val width: Int = if (height == 0) 0 else contents(0).length
 
-  def above(that: Element): Element = new ArrayElement(this.contents ++ that.contents)
+  def above(that: Element): Element = elem(this.contents ++ that.contents)
 
-  def beside(that: Element): Element = new ArrayElement(
+  def beside(that: Element): Element = elem(
     for (
       (line1, line2) <- this.contents zip that.contents
     ) yield line1 + line2
   )
 
   override def toString: String = contents.mkString("\n")
+}
+
+object Element {
+  def elem(contents: Array[String]): Element = new ArrayElement(contents)
+
+  def elem(ch: Char, width: Int, height: Int): Element = new UniformElement(ch, width, height)
+
+  def elem(line: String): Element = new LineElement(line)
 }
 
 class ArrayElement(val contents: Array[String]) extends Element
